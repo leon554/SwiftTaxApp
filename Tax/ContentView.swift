ce79bc1f5f2d148087b8128671116393fa6134de
leon smit <leoncsmit@gmail.com>
2025-11-21 12:32:40 +1100

    Initial Commit

---
+++ Tax/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tax
+//
+//  Created by leon smit on 21/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tax/TaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxApp.swift
+//  Tax
+//
+//  Created by leon smit on 21/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct TaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
